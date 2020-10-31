885ee0aa2d1bd4586174e6afba9dd16965e2afd3
Cecil T <seciltore.dc@gmail.com>
2020-10-31 16:47:52 -0400

    "Setter Injection"

---
+++ Spring-02-DI-SetterInjection/src/main/java/com/TeaPot/services/TeaPotApp.java
@@ -0,0 +1,17 @@
+package com.TeaPot.services;
+
+import com.TeaPot.interfaces.Courses;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class TeaPotApp {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        Courses courses = (Courses) container.getBean("java");
+
+        courses.getTeachingHours();
+
+    }
+}
