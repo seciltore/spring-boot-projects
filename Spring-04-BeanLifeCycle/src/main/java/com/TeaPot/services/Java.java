3848cd1cad4d16aef1f8875dbcc76028d2f47ebf
Cecil T <seciltore.dc@gmail.com>
2020-10-31 17:34:26 -0400

    "BeanLife Cycle"

---
+++ Spring-04-BeanLifeCycle/src/main/java/com/TeaPot/services/Java.java
@@ -0,0 +1,11 @@
+package com.TeaPot.services;
+
+import com.TeaPot.interfaces.Courses;
+
+public class Java implements Courses {
+
+
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours : 20");
+    }
+}
