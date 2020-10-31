cc28373290ffd5e37a6cbcddd1f2c33ea82055f1
Cecil T <seciltore.dc@gmail.com>
2020-10-31 14:46:03 -0400

    Some notes added about Spring framework as basic knowledge

--- Spring-00-LooselyCoupled/src/TeaPotApp.java
+++ Spring-00-LooselyCoupled/src/TeaPotApp.java
@@ -8,11 +8,50 @@ public class TeaPotApp {
 
         FullTimeEmployee fullTime = new FullTimeEmployee();
      //   PartTimeEmployee partTime = new PartTimeEmployee();
-        //new is not preferable, new always create new dependency. Sprint fm has advantage of it.
+        //new is not preferable, new always create new dependency.
+        // Sprint fm has advantage of it. inversionOfControl IoC, create beans for next step
 
         EmployeeAccount employee = new EmployeeAccount(fullTime);
         employee.manageAccount();
-
         }
+
+    /*
+     There are 2 types of IoC Containers:
+     1. Spring BeanFactory Container
+     2. Spring ApplicationContext Container
+
+     Spring BeanFactory Container:
+      it is an interface, coming from springframework
+      It provides basic functionality
+      implementation classes: XmlBeanFactory, DefaultListableBeanFactory, SimpleJndiBeanFactory
+
+     Spring ApplicationContext Container:
+      It is interface
+      Includes all the functionalities of BeanFactory
+      Implementation classes: FileSystemXmlApplicationContext, ClassPathXmlApplicationContext, WebXmlApplicationContext
+
+                                           BeanFactory Interface
+                                                    ^
+                                                    |     (extends)
+                                      ApplicationContext Interface
+                                  ^                 ^                    ^
+                                 /                  |                     \    (implements)
+   ClassPathXmlApplicationContext      FileSystemXmlApplicationContext    AnnotationConfigApplicationContext
+              class                          Class                                Class
+
+
+      Note:
+          Bean is simply a Java object
+          Bean is an object that is created and managed by Spring Container
+          Beans are created from normal Java classes
+
+
+
+
+
+     */
+    }
+
+     */
 
 }
