9c74fd128f95173729c499358818e5b8ca53907c
Cecil T <seciltore.dc@gmail.com>
2020-11-07 12:31:15 -0500

    "JavaCode-Configuration"

--- Spring-06-JavaCode-Configuration/src/main/java/com/TeaPot/configs/TeaPotAppConfig.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/TeaPot/configs/TeaPotAppConfig.java
@@ -1,10 +1,17 @@
 package com.TeaPot.configs;
 
+import com.TeaPot.services.Java;
+import com.TeaPot.services.Selenium;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.TeaPot")
 public class TeaPotAppConfig{
+
+    //Another way to create bean, NO need to write @Component in Java and Selenium classes
+    //@Bean public Java java(){ return new Java();}
+    //@Bean public Selenium selenium(){ return new Selenium();}
 
 }
