e7b03260a7c6a6c97e2010a31adda17de7d88e8b
Cecil T <seciltore.dc@gmail.com>
2020-11-23 13:59:35 -0500

    "SpringBoot MVC-Controller"

---
+++ Spring-08-SpringBoot-MVC-View(Thymeleaf)/src/main/java/com/teapot/model/Student.java
@@ -0,0 +1,17 @@
+package com.teapot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+}
