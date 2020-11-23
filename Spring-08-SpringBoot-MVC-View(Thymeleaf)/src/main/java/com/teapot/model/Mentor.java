591e12c8c0919384c8b261fa9e830924fc198236
Cecil T <seciltore.dc@gmail.com>
2020-11-23 14:25:16 -0500

    "SpringBoot MVC Controller(ThymeLeaf)"

---
+++ Spring-08-SpringBoot-MVC-View(Thymeleaf)/src/main/java/com/teapot/model/Mentor.java
@@ -0,0 +1,19 @@
+package com.teapot.model;
+
+import com.teapot.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Mentor {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Gender gender;
+}
