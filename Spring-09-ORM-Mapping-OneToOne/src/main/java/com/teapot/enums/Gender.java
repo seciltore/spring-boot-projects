c47d4f1165f53ebc2b1d300688c9dba592ef66be
Cecil T <seciltore.dc@gmail.com>
2020-12-24 16:22:51 -0500

    ORM Mapping - OneToOne

---
+++ Spring-09-ORM-Mapping-OneToOne/src/main/java/com/teapot/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.teapot.enums;
+
+public enum Gender {
+    M, F
+}
---
+++ Spring-09-ORM-Mapping-OneToOne/src/main/java/com/teapot/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.teapot.repository;
+
+import com.teapot.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+}
