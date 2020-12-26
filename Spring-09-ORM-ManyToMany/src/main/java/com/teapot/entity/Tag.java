768dfd57ca6081f5d3ddb51af349ceccee558c8c
Cecil T <seciltore.dc@gmail.com>
2020-12-26 11:51:12 -0500

    ORM Mapping - ManyToMany

---
+++ Spring-09-ORM-ManyToMany/src/main/java/com/teapot/entity/Tag.java
@@ -0,0 +1,25 @@
+package com.teapot.entity;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "tags", cascade = CascadeType.ALL)
+    private Set<Post> posts = new HashSet<>();
+
+    public Tag(String name) {
+        this.name = name;
+    }
+}
