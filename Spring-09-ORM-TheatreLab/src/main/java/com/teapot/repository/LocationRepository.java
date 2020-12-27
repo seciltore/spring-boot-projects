842d94ce7d2bb0c4856776a4ed065e9a13d8cc65
Cecil T <seciltore.dc@gmail.com>
2020-12-27 13:33:32 -0500

    ORM - Theatre App

--- Spring-09-ORM-TheatreLab/src/main/java/com/teapot/repository/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.teapot.repository;
-
-import com.teapot.entity.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
