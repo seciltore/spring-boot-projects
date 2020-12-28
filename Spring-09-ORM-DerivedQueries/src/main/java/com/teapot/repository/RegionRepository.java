ef5151b92212c35308f2fd7f0549193397dc8149
Cecil T <seciltore.dc@gmail.com>
2020-12-28 17:52:22 -0500

    ORM - DerivedQuery

---
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/teapot/repository/RegionRepository.java
@@ -0,0 +1,28 @@
+package com.teapot.repository;
+
+import com.teapot.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface RegionRepository extends JpaRepository<Region, Integer> {
+
+    //Display all regions in Canada
+    List<Region> findByCountry(String country);
+
+    //Display all regions in Canada, without duplicates
+    List<Region> findDistinctByCountry(String country);
+
+    //Display all regions with country name includes 'United'
+    List<Region> findByCountryContaining(String country);
+
+    //Display all regions with country name includes 'United' in order
+    List<Region> findByCountryContainingOrderByCountry(String country);
+
+    //Display top 2 regions in Canada
+    List<Region> findTop2ByCountry(String country);
+
+
+
+}
