df2db5a2181e383944e3498d9459aeb152807147
Cecil T <seciltore.dc@gmail.com>
2020-12-29 21:33:44 -0500

    ORM - Named Queries

--- Spring-09-Mapping-JPQL-NamedQueries/src/main/java/com/teapot/entity/Department.java
+++ Spring-09-Mapping-JPQL-NamedQueries/src/main/java/com/teapot/entity/Department.java
@@ -6,9 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "departments")
@@ -16,6 +14,11 @@ import javax.persistence.Table;
 @Setter
 @NoArgsConstructor
 @ToString
+@NamedQuery(name="Department.findSecilDepartment",
+        query = "SELECT d FROM Department d WHERE d.division=?1")
+@NamedNativeQuery(name="Department.countAllDepartments",
+        query="SELECT count(*) FROM departments",
+        resultClass = Department.class)
 public class Department {
     @Id
     private String department;
