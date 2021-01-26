49ae1f6b025665a2b5e07f545c6f556f05d75712
seciltore <seciltore.dc@gmail.com>
2021-01-26 18:54:52 -0500

    "Security Http Basic AuthRoleBase"

---
+++ Spring-10-Security-HttpBasic-AutoRoleBase/src/main/java/com/security3/security/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.security3.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests() //request should be authorized
+                .anyRequest().authenticated() //incoming request be authenticated
+                .and()
+                .httpBasic(); //perform basic http authentication
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN")
+                .and()
+                .withUser("secil").password(passwordEncoder().encode("secil")).roles("USER");
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
