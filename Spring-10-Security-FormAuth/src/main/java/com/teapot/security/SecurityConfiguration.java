e88cb5a4926d4d8b85b49787363efd2cadefe66e
seciltore <seciltore.dc@gmail.com>
2021-01-27 10:36:50 -0500

    Security-FormAuth

--- Spring-10-Security-FormAuth/src/main/java/com/teapot/security/SecurityConfiguration.java
+++ Spring-10-Security-FormAuth/src/main/java/com/teapot/security/SecurityConfiguration.java
@@ -24,6 +24,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login")
                 .defaultSuccessUrl("/index")
+                .failureUrl("/login?error=true")
                 .permitAll();
     }
 
--- Spring-10-Security-FormAuth/src/main/java/com/teapot/security/UserPrincipalDetailsService.java
+++ Spring-10-Security-FormAuth/src/main/java/com/teapot/security/UserPrincipalDetailsService.java
@@ -19,6 +19,9 @@ public class UserPrincipalDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(s);
+        if(user==null){
+            throw new UsernameNotFoundException("user doesn't exist");
+        }
         UserPrincipal userPrincipal = new UserPrincipal(user);
 
         return userPrincipal;
