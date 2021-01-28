4afb6ff7b0e69a70f5ef1180192a349154e6bb79
seciltore <seciltore.dc@gmail.com>
2021-01-28 12:04:44 -0500

    Rest-HttpVerbs-Legacy

--- Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/teapot/controller/ProductController.java
+++ Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/teapot/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.teapot.controller;
 import com.teapot.entity.Product;
 import com.teapot.service.ProductService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +24,21 @@ public class ProductController {
     @RequestMapping(value = "/products")
     public @ResponseBody List<Product> getProducts(){
         return productService.getProducts();
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public @ResponseBody List<Product> createProduct(@RequestBody Product product){
+        return productService.createProduct(product);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+    public @ResponseBody List<Product> deleteProduct(@PathVariable("id") long id){
+        return productService.delete(id);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+    public @ResponseBody List<Product> updateProduct(@PathVariable("id") long id, @RequestBody Product product){
+        return productService.updateProduct(id, product);
     }
 
 
