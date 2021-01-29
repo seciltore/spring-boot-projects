f517dc04bbcf27b8200f7f93d57444a18159c95f
seciltore <seciltore.dc@gmail.com>
2021-01-29 17:31:52 -0500

    Rest-SchoolLab

--- Spring-11-Rest-SchoolLab/src/main/java/com/school/model/Teacher.java
+++ Spring-11-Rest-SchoolLab/src/main/java/com/school/model/Teacher.java
@@ -1,4 +1,45 @@
 package com.school.model;
 
-public class Teacher {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.school.enums.EducationLevel;
+import com.school.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "teacher")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(value={"hibernateLazyInitializer"},ignoreUnknown = true)
+public class Teacher extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    private LocalDate birthday;
+
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "education_level")
+    private EducationLevel educationLevel;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
 }
