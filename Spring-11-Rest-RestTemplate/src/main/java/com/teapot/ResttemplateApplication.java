db86d9c582d6bccb75ca3ff88b679885ad6c26a9
seciltore <seciltore.dc@gmail.com>
2021-01-29 16:47:45 -0500

    Rest-RestTemplate

---
+++ Spring-11-Rest-RestTemplate/src/main/java/com/teapot/ResttemplateApplication.java
@@ -0,0 +1,27 @@
+package com.teapot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@SpringBootApplication
+public class ResttemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResttemplateApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder
+                .setConnectTimeout(Duration.ofMillis(2000))
+                .setReadTimeout(Duration.ofMillis(3000))
+                .build();
+
+    }
+
+}
