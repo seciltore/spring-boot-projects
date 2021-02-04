1a5d556a316bbef0eff566d300fe56c2d039a2e7
seciltore <seciltore.dc@gmail.com>
2021-02-03 20:19:24 -0500

    Rest-OpenApi3.0

---
+++ Spring-11-Rest-OpenApi3.0/src/main/java/com/teapot/OpenapiapplicationApplication.java
@@ -0,0 +1,25 @@
+package com.teapot;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class OpenapiapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenapiapplicationApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI customOpenApi(){
+
+        return new OpenAPI()
+                .info(new Info().title("Cinema Application")
+                .version("v1")
+                .description("Selling online cinema tickets")
+                );
+    }
+}
