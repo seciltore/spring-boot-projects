431417bd636baed8f8818ce66b9181e61da2a052
seciltore <seciltore.dc@gmail.com>
2021-02-08 20:15:05 -0500

    Rest-Security-JWT

---
+++ Spring-11-Rest-Security-JWT/src/main/java/com/teapot/service/SecurityFilter.java
@@ -0,0 +1,17 @@
+package com.teapot.service;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SecurityFilter {
+}
---
+++ Spring-11-Rest-Security-JWT/src/main/java/com/teapot/util/JWTUtil.java
@@ -0,0 +1,41 @@
+package com.teapot.util;
+
+import com.teapot.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JWTUtil {
+
+    @Value("${security.jwt.secret-key}")
+    private String secret = "teapot";
+
+    public String generateToken(User user, String username){
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", user.getUsername());
+        claims.put("email", user.getEmail());
+
+        return createToken(claims, username);
+    }
+
+    private String createToken(Map<String, Object> claims, String username){
+
+        return Jwts
+                .builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*60*3)) //3 hours
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+
+    }
+
+}
